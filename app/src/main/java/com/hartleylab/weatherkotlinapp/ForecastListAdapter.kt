f71443dbeec5c333f43ca82d5db8952191875833
Vishesh Jindal <vishesh@hartleylab.com>
2019-04-26 17:27:09 +0530

    * Added Recycler View * Added Adapter * Added Internet permission * Added Request Class * Added Forecast data class

---
+++ app/src/main/java/com/hartleylab/weatherkotlinapp/ForecastListAdapter.kt
@@ -0,0 +1,21 @@
+package com.hartleylab.weatherkotlinapp
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+}
